af3d9efbc51c86067bf7db65179b6e2d20c2b132
lwx <1109302358@qq.com>
2019-07-25 15:50:07 +0800

    哈希映射,设计建

--- src/hash/Run.java
+++ src/hash/Run.java
@@ -1,10 +1,16 @@
 package hash;
 
+import hash.design_key.Anagrams;
 import hash.hash_map.CommonRestaurant;
+import hash.hash_map.DuplicateNum;
+import hash.hash_map.SingleCharacter;
 import hash.hash_set.Duplicate;
 import hash.hash_set.HappyNumber;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class Run {
     @Test
@@ -33,5 +39,51 @@ public class Run {
         CommonRestaurant restaurant = new CommonRestaurant();
 
         String[] result = restaurant.findRestaurant(list1, list2);
+    }
+
+    @Test
+    public void singleChar(){
+        SingleCharacter singleCharacter = new SingleCharacter();
+
+        String s = "cc";
+        int index = singleCharacter.firstUniqChar(s);
+        System.out.println(index);
+    }
+
+    @Test
+    public void dup(){
+        DuplicateNum duplicateNum = new DuplicateNum();
+
+        int[] nums = {1,2,3,1,2,3};
+        int k = 2;
+
+        boolean result = duplicateNum.containsNearbyDuplicate(nums, k);
+        System.out.println(result);
+    }
+
+    @Test
+    public void anagram(){
+        Anagrams anagrams = new Anagrams();
+
+        String[] strs = {"tao","pit","cam","aid","pro","dog"};
+
+        List<List<String>> result = anagrams.groupAnagrams(strs);
+
+        for (int i = 0; i < result.size(); i++){
+            for (int j = 0; j < result.get(i).size(); j++){
+                System.out.print(result.get(i).get(j) + " ");
+            }
+            System.out.println(" ");
+        }
+    }
+
+    @Test
+    public void test(){
+        String s = "hello";
+        char[] ca = s.toCharArray();
+        Arrays.sort(ca);
+        String key = String.valueOf(ca);
+
+        System.out.println(key);
     }
 }
---
+++ src/hash/hash_map/SingleCharacter.java
@@ -0,0 +1,63 @@
+package hash.hash_map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 给定一个字符串，找到它的第一个不重复的字符，并返回它的索引。如果不存在，则返回 -1。
+ *
+ * s = "leetcode"
+ * 返回 0.
+ *
+ * s = "loveleetcode",
+ * 返回 2.
+ *
+ */
+public class SingleCharacter {
+
+    /**
+     * 使用哈希映射，巨慢无比
+     * @param s
+     * @return
+     */
+    public int firstUniqChar(String s) {
+        if (s == null || s.length() == 0){
+            return -1;
+        }
+
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++){
+            if (map.containsKey(s.charAt(i))){
+                int num = map.get(s.charAt(i));
+                map.replace(s.charAt(i), num, num+1);
+            }else {
+                map.put(s.charAt(i), 1);
+            }
+        }
+
+        for (int i = 0; i < s.length(); i++){
+            if (map.get(s.charAt(i)) == 1){
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    /**
+     * 使用双索引
+     * @param s
+     * @return
+     */
+    public int firstUniqChar2(String s) {
+        int index = -1;
+        for (char ch = 'a'; ch <= 'z'; ch++) {
+            int startIndex = s.indexOf(ch);
+            if (startIndex != -1 && startIndex == s.lastIndexOf(ch)) {
+                index = (index == -1 || index > startIndex) ? startIndex : index;
+            }
+        }
+        return index;
+    }
+}
