3a68c4ac9fe115ca24f5f08a7701f6b19056137d
FYBEI <1109302358@qq.com>
2020-11-20 13:21:11 +0800

    算法作业1和3整合

--- src/HW/HW3/HW3_2.java
+++ src/HW/HW3/HW3_2.java
@@ -2,49 +2,89 @@ package HW.HW3;
 
 public class HW3_2 {
 
-    public static float OptBST(float[] P){
-        int n = P.length;
-        float[][] E = new float[n][n];
-        float[][] R = new float[n][n];
+    //接受一个中序序列的点的查找概率数组，返回最优的二叉查找树的代价（注意P中的概率按顺序对应于点的中序序列）
+    public static float[][] OptBST(float[] P){
 
-        for (int i = 1; i <= n; i++)  //初始化
-        {
-            E[i][i-1] = P[i-1];
-            E[i][i] = P[i];
-            R[i][i] = i;
+        int n = P.length; //结点个数
+        float[][] result = new float[n][n];
+
+        int[][] R = new int[n][n]; //表达二叉查找树形状的矩阵
+
+        for(int i = 0; i < n; i++) {
+            result[i][i] = P[i]; //填充主对角线C[i,i] = P[i]
+            R[i][i] = i; //R[i][j]表示若只构造从i到j的树，那么root是R[i][j]
         }
-        E[n+1][n]=0;
 
-        for (int d = 1; d < n; d++)     //按对角线逐条计算
-            for (int i=1; i <= n-d; i++)
-            {
-                int j = i + d;
-                float min = Float.MAX_VALUE;
-                int mink = i;
-                float sum = 0;
-                for (int k = i; k <= j; k++)
-                {
-                    sum = sum + P[k];
-                    if (E[i][k-1] + E[k+1][j] < min) {
-                        min = E[i][k-1] + E[k+1][j];
-                        mink=k;
+        for(int d = 1; d <= n - 1; d++) {
+            //共n-1条对角线需要填充
+
+            for(int i = 0; i <= n - d - 1; i++) {
+                //横坐标的范围与对角线编号d的关系
+
+                int j = i + d; //一旦横坐标确定后，纵坐标可以用横坐标与对角线编号表示出来
+                float min = Integer.MAX_VALUE;
+
+                int root = 0;
+
+                for(int k = i; k <= j; k++) {
+
+                    float C1 = 0, C2 = 0; //用C1，C2表示result[i,k-1]和result[k+1,j]
+                    if(k > i)
+                        C1 = result[i][k - 1];
+                    if(k < j)
+                        C2 = result[k + 1][j];
+
+                    if(C1 + C2 < min) {
+                        min = C1 + C2;
+                        root = k;
                     }
                 }
-                E [i][j]=min+sum;
-                R[i][j]=mink;
-            }
 
-        return E[1][n];
+                R[i][j] = root; //R[i][j]的值代表从i到j的最优二叉查找树的根
+
+                float sum = 0;
+                for(int s = i; s <= j; s++)
+                    sum = sum + P[s];
+
+                result[i][j] = sum + min;
+            }
+        }
+
+//        return result[0][n-1]; //返回C[1,n],最小代价
+//        return R; //返回表达最优二叉排序树形状的矩阵
+        return result;
     }
 
     public static void main(String[] args) {
 
         float[] P = {(float) 0.05, (float) 0.15, (float) 0.1, (float) 0.1, (float) 0.05, (float) 0.05, (float) 0.05, (float) 0.1, (float) 0.05, (float) 0.2, (float) 0.1};
 
-        System.out.println("输出表达最优二叉排序树形状的矩阵：\n");
-        float result = OptBST(P);
+        //若返回值是最小代价，测试最小代价是否正确
+//        System.out.print("输出最优二叉排序树的最小代价：");
+//        float result = OptBST(P);
+//        System.out.println(result);
 
-        System.out.println(result);
+        //若返回值是表达最优二叉排序树形状的矩阵，测试矩阵是否正确
+//        System.out.println("输出表达最优二叉排序树形状的矩阵：\n");
+//        int[][] R = OptBST(P);
+//        for(int i = 0; i < R.length; i++) {
+//
+//            for(int j = 0; j < R.length; j++)
+//                System.out.print(R[i][j] + "　　");
+//
+//            System.out.println();
+//        }
+
+        //若返回值是代表子树权重吗，测试矩阵是否正确
+        System.out.println("输出表达最优二叉排序树权重的矩阵：\n");
+        float[][] result = OptBST(P);
+        for(int i = 0; i < result.length; i++) {
+
+            for(int j = 0; j < result.length; j++)
+                System.out.printf("%.2f\t", result[i][j]);
+
+            System.out.println();
+        }
 
     }
 }
