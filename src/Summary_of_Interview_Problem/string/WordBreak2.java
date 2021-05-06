fc35e58cfbed4e718bd6d2666e84e0f16addb029
lwx <1109302358@qq.com>
2021-05-06 23:13:35 +0800

    单词拆分2

--- src/Summary_of_Interview_Problem/string/WordBreak2.java
+++ src/Summary_of_Interview_Problem/string/WordBreak2.java
@@ -42,17 +42,28 @@ public class WordBreak2 {
 
         Set<String> wordDictSet = new HashSet(wordDict);
 
-        boolean[][] dp = new boolean[s.length()+1][s.length()+1];   //dp[i][j]表示s从i-j的子串是否为单词
+        //dp[i][j]表示s从i-j的子串是否为单词
+        boolean[][] dp = new boolean[s.length()+1][s.length()+1];
 
         for (int i = 0; i <= s.length(); i++){
             for (int j = i+1; j <= s.length(); j++){
-                 if (wordDictSet.contains(s.substring(i, j)))
+                 if (wordDictSet.contains(s.substring(i, j))){
                      dp[i][j] = true;
-                 else
+                 } else {
                      dp[i][j] = false;
+                 }
             }
         }
 
+        for (int i = 0; i <= s.length(); i++){
+            for (int j = 0; j <= s.length(); j++){
+                System.out.print(dp[i][j]);
+                System.out.printf(" ");
+            }
+            System.out.println();
+        }
+
+        // 获得所有单词排列情况
         List<List<Integer>> indexes = dfs(dp, 0, s);
 
         for (List<Integer> index : indexes){
@@ -61,24 +72,65 @@ public class WordBreak2 {
 
         List<String> sents = new ArrayList<>();
 
+        for (List<Integer> index : indexes){
+            // 如果拆分的最后一位不是字符串长度，说明字符串未完全拆分
+            if (index.get(index.size()-1) != s.length()){
+                continue;
+            }
+
+            //起始拆分位置为0
+            int begin = 0;
+
+            StringBuilder sent = new StringBuilder();
+            for (int i : index){
+
+                // 起始位置不为开头的话需要用空格隔开
+                if (begin != 0){
+                    sent.append(" ");
+                }
+
+                //拆分截至位置
+                int end = i;
+
+                String word = s.substring(begin, end);
+                sent.append(word);
+
+                // 下一个词的起始位置为当前的拆分截至位置
+                begin = i;
+            }
+
+            sents.add(sent.toString());
+        }
 
         return sents;
     }
 
+    // 利用递归，获得全部的可拆分位置
     private List<List<Integer>> dfs(boolean[][] dp, int i, String s){
 
+        // 记录所有单词排列情况
         List<List<Integer>> indexes = new ArrayList<>();
-        for (int j = i+1; j < s.length(); j++){
+        for (int j = i+1; j <= s.length(); j++){
             if (dp[i][j]){
+                List<Integer> index = new ArrayList<>();
+                index.add(j);
 
                 List<List<Integer>> next_indexes = dfs(dp, j, s);
-                for (List<Integer> next_index : next_indexes){
-                    List<Integer> index = new ArrayList<>();
 
-                    index.add(j);
-                    index.addAll(next_index);
-
+                if (next_indexes.size() == 0){
+                    //说明下一项已经没有单词排列
                     indexes.add(index);
+                }else if (next_indexes.size() == 1){
+                    //下一项只有一种单词排列情况
+                    index.addAll(next_indexes.get(0));
+                    indexes.add(index);
+                }else {
+                    //下一项有多种单词排列情况
+                    for (List<Integer> next_index : next_indexes){
+                        List<Integer> new_index = new ArrayList<>(index);
+                        new_index.addAll(next_index);
+                        indexes.add(new_index);
+                    }
                 }
 
             }
@@ -92,13 +144,15 @@ public class WordBreak2 {
 
         List<String> wordDict = new ArrayList<>();
         wordDict.add("cats");
-        wordDict.add("sand");
-        wordDict.add("cat");
-        wordDict.add("and");
         wordDict.add("dog");
+        wordDict.add("sand");
+        wordDict.add("and");
+        wordDict.add("cat");
 
         String str = "catsanddog";
 
-        wb2.wordBreak(str, wordDict);
+        List<String> result = wb2.wordBreak(str, wordDict);
+
+        System.out.print(result);
     }
 }
