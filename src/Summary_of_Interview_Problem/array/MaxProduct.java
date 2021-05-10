f5f11dcf987e15c24da4e525e5831260d02610e9
FYBEI <1109302358@qq.com>
2021-05-10 18:54:48 +0800

    面试算法总结

---
+++ src/Summary_of_Interview_Problem/array/MaxProduct.java
@@ -0,0 +1,38 @@
+package Summary_of_Interview_Problem.array;
+
+/***
+ * 给你一个整数数组 nums，请你找出数组中乘积最大的连续子数组（该子数组中至少包含一个数字），并返回该子数组所对应的乘积。
+ *
+ * 示例 1:
+ * 输入: [2,3,-2,4]
+ * 输出: 6
+ * 解释:子数组 [2,3] 有最大乘积 6。
+ *
+ * 示例 2:
+ * 输入: [-2,0,-1]
+ * 输出: 0
+ * 解释:结果不能为 2, 因为 [-2,-1] 不是子数组。
+ */
+public class MaxProduct {
+
+    // fmax = Max(fmax(i-1)*ai, fmin(i-1)*ai, ai)
+    // fmin = Min(fmin(i-1)*ai, fmax(i-1)*ai, ai)
+    public int maxProduct(int[] nums) {
+        int maxF = nums[0], minF = nums[0], ans = nums[0];
+        int length = nums.length;
+        for (int i = 1; i < length; ++i) {
+            int mx = maxF, mn = minF;
+            maxF = Math.max(mx * nums[i], Math.max(nums[i], mn * nums[i]));
+            minF = Math.min(mn * nums[i], Math.min(nums[i], mx * nums[i]));
+            ans = Math.max(maxF, ans);
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2,3,-2,4};
+        MaxProduct maxProduct = new MaxProduct();
+
+        System.out.println(maxProduct.maxProduct(arr));
+    }
+}
