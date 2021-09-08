57747b24219beddfd0c36ec401a1539763c82a72
FYBEI <1109302358@qq.com>
2021-09-08 22:02:33 +0800

    面试算法总结

---
+++ src/Summary_of_Interview_Problem/list/ListNode.java
@@ -0,0 +1,9 @@
+package Summary_of_Interview_Problem.list;
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
