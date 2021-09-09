662a26a721ea1ca138a7b7e5e39eb62bdecc2e53
lwx <1109302358@qq.com>
2021-09-09 21:48:09 +0800

    每日算法题

--- src/linklist/double_pointer/ListNode.java
+++ src/linklist/double_pointer/ListNode.java
@@ -4,9 +4,14 @@ public class ListNode {
     public int val;
     public ListNode next;
 
+    public ListNode() {}
     public ListNode(int x) {
         val = x;
         next = null;
+    }
+    public ListNode(int x, ListNode next) {
+        val = x;
+        this.next = next;
     }
 
 }
