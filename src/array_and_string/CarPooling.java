1e4b94eb54333450c55edad35b2afef578955ff1
FYBEI <1109302358@qq.com>
2021-09-16 23:28:24 +0800

    每日算法题

--- src/array_and_string/CarPooling.java
+++ src/array_and_string/CarPooling.java
@@ -1,57 +1,39 @@
 package array_and_string;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class CarPooling {
     public boolean carPooling(int[][] trips, int capacity) {
 
-        Map<Integer, Integer> start = new HashMap<>();
-        Map<Integer, Integer> end = new HashMap<>();
-
-        int early = Integer.MAX_VALUE;
-        int endest = Integer.MIN_VALUE;
-
-        for (int i = 0; i < trips.length; i++){
-            int num = trips[i][0];
-            int s = trips[i][1];
-            int e = trips[i][2];
-
-            if (!start.containsKey(s)){
-                start.put(s, num);
-            }else {
-                start.replace(s, start.get(s) + num);
-            }
-
-            if (!end.containsKey(e)){
-                end.put(e, num);
-            }else {
-                end.replace(e, end.get(e) + num);
-            }
-
-
-            if (early > s){
-                early = s;
-            }
-            if (endest < e){
-                endest = e;
-            }
+        int[] roadStation = new int[1001];
+        for (int i = 0; i < trips.length; i++) {
+            roadStation[trips[i][1]] += trips[i][0];
+            roadStation[trips[i][2]] -= trips[i][0];
         }
-
-        for (int i = early; i <= endest; i++){
-            if (end.containsKey(i)){
-                capacity += end.get(i);
-            }
-
-            if (start.containsKey(i)){
-                capacity -= start.get(i);
-            }
-
-            if (capacity < 0){
-                return false;
-            }
+        int total = 0;
+        for (int i = 0; i < 1001; i++) {
+            total += roadStation[i];
+            if (total > capacity) return false;
         }
+        return true;
+    }
 
+    public boolean carPooling2(int[][] trips, int capacity) {
+        //创建一个以下车顺序的小根堆
+        PriorityQueue<int[]> heap=new PriorityQueue<>(Comparator.comparingInt(o -> o[2]));
+        //对上车顺序排序
+        Arrays.sort(trips, Comparator.comparingInt(o -> o[1]));
+        for (int[] trip : trips) {
+            //先上车
+            capacity -= trip[0];
+            //车位不够就下车
+            if (capacity < 0) {
+                while (!heap.isEmpty() && heap.peek()[2] <= trip[1]) capacity += heap.poll()[0];//能下的都下完
+                if (capacity < 0) return false;//能下完的 下完还不行就返回
+            }
+            //加到堆里面
+            heap.offer(trip);
+        }
         return true;
     }
 
