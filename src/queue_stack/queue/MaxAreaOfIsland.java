8431d7782f15cdae98271e5ff1d6368f24a86a7a
lwx <1109302358@qq.com>
2021-09-17 17:37:14 +0800

    每日算法题

--- src/queue_stack/queue/MaxAreaOfIsland.java
+++ src/queue_stack/queue/MaxAreaOfIsland.java
@@ -2,33 +2,23 @@ package queue_stack.queue;
 
 public class MaxAreaOfIsland {
     int max;
-    int[][] visit;
 
     public int maxAreaOfIsland(int[][] grid) {
         int n = grid.length;
-        if (grid.length == 0){
+        if (n == 0){
             return 0;
         }
         int m = grid[0].length;
 
-        visit = new int[n][m];
         max = 0;
 
-        for (int i = 0; i < n; i++){
+        for (int i = 0; i < grid.length; i++){
             for (int j = 0; j < m; j ++){
-
-                // 访问过的节点就跳过
-                if (visit[i][j] == 1){
-                    continue;
-                }
 
                 //获取根节点
                 if (grid[i][j] == 1){
 
-                    visit[i][j] = 1;
                     int area = dfs(grid, i, j);
-
-                    // 更新最大面积
                     if (max < area){
                         max = area;
                     }
@@ -41,26 +31,24 @@ public class MaxAreaOfIsland {
 
     // 返回岛屿的面积
     private int dfs(int[][] grid, int i, int j){
+        // 访问过就置0
+        grid[i][j] = 0;
 
         int up = 0, down = 0, left = 0, right = 0;
-        //如果为超界，且上下左右未访问，且为陆地，则遍历上，下，左，右
-        if (i > 0 && visit[i-1][j] == 0 && grid[i-1][j] == 1){
-            visit[i-1][j] = 1;
+        //如果未超界，且上下左右为岛屿，遍历上，下，左，右
+        if (i > 0 && grid[i-1][j] == 1){
             up = dfs(grid, i - 1, j);
         }
 
-        if (i < grid.length-1 && visit[i+1][j] == 0 && grid[i+1][j] == 1){
-            visit[i+1][j] = 1;
+        if (i < grid.length-1 && grid[i+1][j] == 1){
             down = dfs(grid, i + 1, j);
         }
 
-        if (j > 0 && visit[i][j-1] == 0 && grid[i][j-1] == 1){
-            visit[i][j-1] = 1;
+        if (j > 0 && grid[i][j-1] == 1){
             left = dfs(grid, i, j - 1);
         }
 
-        if (j < grid[0].length-1 && visit[i][j+1] == 0 && grid[i][j+1] == 1){
-            visit[i][j+1] = 1;
+        if (j < grid[0].length-1 && grid[i][j+1] == 1){
             right = dfs(grid, i, j + 1);
         }
 
