f6817db78a80416d6aca50451741cc0bd0dd9136
lwx <1109302358@qq.com>
2021-09-24 17:29:35 +0800

    每日算法题

---
+++ src/array_and_string/array/LeastNumbers.java
@@ -0,0 +1,15 @@
+package array_and_string.array;
+
+import java.util.Arrays;
+
+public class LeastNumbers {
+    public int[] getLeastNumbers(int[] arr, int k) {
+        if (k == 0){
+            return new int[0];
+        }
+
+        Arrays.sort(arr);
+
+        return Arrays.copyOf(arr, k);
+    }
+}
