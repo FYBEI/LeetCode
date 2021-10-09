96432f813b4a9439c276fe1798a353d24077d8ec
lwx <1109302358@qq.com>
2021-10-09 15:09:33 +0800

    每日算法题

--- src/search/model2/Peak.java
+++ src/search/model2/Peak.java
@@ -17,46 +17,38 @@ package search.model2;
  */
 public class Peak {
     public int findPeakElement(int[] nums) {
-        if (nums.length == 1){
+        int n = nums.length;
+        if (n <= 1){
             return 0;
         }
 
         int left = 0;
-        int right = nums.length;
+        int right = n-1;
 
-        while (left < right){
-            int mid = left + (right - left)/2;
+        while(left <= right){
+            int mid = (left + right) >> 1;
 
-            if (mid != 0 && mid != nums.length-1){
+            if (mid != 0 && mid != n-1){
                 if (nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]){
                     return mid;
-                }else if (nums[mid] > nums[mid-1] && nums[mid] < nums[mid+1]){
-                    left = mid+1;
-                }else {
-                    right = mid;
+                }else if(nums[mid] > nums[mid-1] && nums[mid] < nums[mid+1]){
+                    left = mid + 1;
+                }else{
+                    right = mid - 1;
                 }
-            }else if (mid == 0){
+            }else if(mid == 0){
                 if (nums[mid] > nums[mid+1]){
                     return mid;
-                }else if (nums[mid] < nums[mid+1]){
-                    left = mid+1;
-                }else {
-                    right = mid;
+                }else{
+                    left = mid + 1;
                 }
-            }else if (mid == nums.length-1){
+            }else if(mid == n-1){
                 if (nums[mid] > nums[mid-1]){
                     return mid;
-                }else if (nums[mid] > nums[mid-1]){
-                    left = mid+1;
-                }else {
-                    right = mid;
+                }else{
+                    right = mid - 1;
                 }
             }
-
-        }
-
-        if (left != nums.length && nums[left] > nums[left-1] && nums[left] > nums[left+1]){
-            return left;
         }
 
         return -1;
