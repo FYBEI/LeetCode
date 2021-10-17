d563eb05e1550f9b8a7f5293d158e3977dbd7970
FYBEI <1109302358@qq.com>
2021-10-17 15:53:12 +0800

    每日算法题

---
+++ src/binary_tree/IsSubtree.java
@@ -0,0 +1,35 @@
+package binary_tree;
+
+public class IsSubtree {
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if (root == null && subRoot == null){
+            return true;
+        }
+
+        if (root == null || subRoot == null){
+            return false;
+        }
+
+        if (root.val == subRoot.val && isSub(root, subRoot)){
+            return true;
+        }else {
+            return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+        }
+    }
+
+    private boolean isSub(TreeNode root, TreeNode subRoot){
+        if (root == null && subRoot == null){
+            return true;
+        }
+
+        if (root == null || subRoot == null){
+            return false;
+        }
+
+        if (root.val == subRoot.val){
+            return isSub(root.left, subRoot.left) && isSub(root.right, subRoot.right);
+        }else {
+            return false;
+        }
+    }
+}
