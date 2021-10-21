f5bb14445ef45954e13977d52d654092d99e8ab1
lwx <1109302358@qq.com>
2021-10-21 11:22:31 +0800

    每日算法题

---
+++ src/queue_stack/CanReach.java
@@ -0,0 +1,31 @@
+package queue_stack;
+
+public class CanReach {
+    boolean[] vis;
+
+    public boolean canReach(int[] arr, int start) {
+        vis = new boolean[arr.length];
+
+        return reach(arr, start);
+    }
+
+    private boolean reach(int[] arr, int start) {
+        if (start >= arr.length || start < 0){
+            return false;
+        }
+
+        if (vis[start]){
+            return false;
+        }
+
+        if (arr[start] == 0){
+            return true;
+        }
+
+        vis[start] = true;
+        boolean next1 = reach(arr, start + arr[start]);
+        boolean next2 = reach(arr, start - arr[start]);
+
+        return next1 || next2;
+    }
+}
